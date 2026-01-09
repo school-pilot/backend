1decdcc4b275a6954c8b96ff2c34f0fa36fa7900
Junior's Signature <chibuzor.john.2018@gmail.com>
2026-01-09 19:58:26 +0100

    Add localhost:5170 to CORS allowed origins

--- sms_prj/settings.py
+++ sms_prj/settings.py
@@ -356,6 +356,7 @@ CORS_ALLOWED_ORIGINS = [
     'http://localhost:8000',
     'http://127.0.0.1:3000',
     'http://127.0.0.1:8000',
+    'http://localhost:5170',
 ]
 if custom_domain := os.getenv('CUSTOM_DOMAIN'):
     CORS_ALLOWED_ORIGINS.append(f'https://{custom_domain}')
